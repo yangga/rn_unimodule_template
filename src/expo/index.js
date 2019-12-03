1c7135334b61300c9687a18cfcedf0eff6029602
Jake Yang <jakeyang@Jakeui-MacBook-Pro.local>
2019-12-03 18:00:28 +0900

    updated expo-keep-awake apis

--- src/expo/index.js
+++ src/expo/index.js
@@ -1,7 +1,7 @@
 // ref : https://docs.expo.io/versions/latest/bare/unimodules-full-list/
 
-export {default as AppLoading} from './AppLoading'
+export { default as AppLoading } from './AppLoading'
 export { Asset } from 'expo-asset';
-export {default as Constants} from './Constants';
+export { default as Constants } from './Constants';
 export * as FileSystem from 'expo-file-system';
-export {default as KeepAwake} from 'expo-keep-awake';
+export { activateKeepAwake, deactivateKeepAwake, useKeepAwake } from 'expo-keep-awake';
